ab1ce1bd70db71f4922874ce6e636d714466bb32
PorkShoulderHolder <sfoxroyston@gmail.com>
2015-05-04 20:00:35 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-scp main.lua data.lua a4_communication_loop.lua generate.lua a4_vocab.lua try.lua base.lua a4_grading.py base.lua Sam@169.53.62.118:/home/Sam/lstm
+scp main.lua data.lua a4_communication_loop.lua generate.lua a4_vocab.lua a4_grading.py base.lua Sam@169.53.62.118:/home/Sam/lstm
